7a32acdd400533b57c37fc212378caf61222dd68
annielepiere <annalepiere@gmail.com>
2024-02-26 16:51:43 -0800

    start Page Table

--- PageTable.py
+++ PageTable.py
@@ -1,18 +1,36 @@
 class PageTable:
 
     #2^8 page table entries
-    def __init__(self, page_size, num_pages, num_frames):
-        self.page_size = 25
-        self.num_pages = num_pages
+    def __init__(self, num_pages, num_frames):
+        self.page_size = 256
         self.num_frames = num_frames
-        self.page_table = [None] * num_pages
+        self.num_pages = num_pages
         self.size = 256
+        self.entries = {} # List of tuples (page #, frame #)
 
-    # def get_frame(self, page_num):
-    #     return self.page_table[page_num]
+    def __init__(self):
+        self.size = 16
+        self.entries ={}  # List of tuples (page #, frame #)
 
-    # def set_frame(self, page_num, frame_num):
-    #     self.page_table[page_num] = frame_num
+    def lookup(self, page):
+        return self.entries.get(page, None) # Return the physical frame if it exists, else None
+        
+    def insert(self, page, frame):
+        if self.isFull():
+            self.entries.pop(next(iter(self.entries)))   # Remove the oldest entry (FIFO)
+        self.entries[page] = frame  # Add the new entry to the end
 
-    # def __str__(self):
-    #     return str(self.page_table)
+    def isFull(self):
+        return len(self.tlb) == self.size 
+    
+    def delete(self, frame):
+        for page, frame in self.entries.items():
+            if frame == frame:
+                self.entries.pop(page)
+                break
+
+    def size(self):
+        return len(self.entries)
+    
+    def contains(self, page):
+        return page in self.entries
