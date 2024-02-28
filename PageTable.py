e56aaceba9122416cc3cfcd70d2cdba4664687c6
bhillyard <bhillyar@calpoly.edu>
2024-02-27 23:29:30 -0800

    roughly working, needs testing

--- PageTable.py
+++ PageTable.py
@@ -44,9 +44,26 @@ class PageTable:
                          break 
                          #do nothing if it is FIFO because it is already in position it should be for replacement order
 
-    def getVictim(self):
-        victim = self.reference_Queue.get()
-        #self.entries[victim] = None
+    def getVictim(self, page_numbers, index, opt_set):
+        if self.pra == 'FIFO' or self.pra == 'LRU':
+            victim = self.reference_Queue.get()
+        else:
+            #check every loaded in frame to see which one will be used further in the future for OPT
+            print("CHECKING OPT LIST")
+            print(page_numbers[index:])
+            for key in opt_set:
+                distance = 0
+                for page_num in page_numbers[index:]:
+                    print(distance, " for ", key)
+                    if key == page_num:
+                        opt_set[key] = distance
+                        break
+                    distance += 1
+                #apparently you can make an else statement to run if for loop doesnt exit with a break
+                else:
+                    opt_set[key] = distance
+            victim = max(opt_set, key=opt_set.get)
+            print("REMOVING VICTIM", victim, " WITH SCORE ", opt_set[victim])
         return victim
 
     def isFull(self):
--- TLB.py
+++ TLB.py
@@ -1,27 +1,30 @@
+from queue import Queue
+
+
 class TLB:
     def __init__(self):
         self.size = 16
         self.entries ={}  # List of tuples (page #, frame #)
+        self.reference_Queue = Queue()
+
 
     def lookup(self, page):
         return self.entries.get(page, None) # Return the physical frame if it exists, else None
         
     def insert(self, page, frame):
         if self.isFull():
-            self.entries.pop(next(iter(self.entries)))   # Remove the oldest entry (FIFO)
+            self.entries.pop(self.reference_Queue.get())
+        self.reference_Queue.put(page)
         self.entries[page] = frame  # Add the new entry to the end
 
     def isFull(self):
-        return len(self.tlb) == self.size 
-    
-    def delete(self, frame):
-        for page, frame in self.entries.items():
-            if frame == frame:
-                self.entries.pop(page)
-                break
+        return len(self.entries) == self.size 
 
     def size(self):
         return len(self.entries)
     
     def contains(self, page):
         return page in self.entries
+
+    def printTable(self):
+        print(self.entries)
